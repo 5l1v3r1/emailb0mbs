b4c7fa33ab0a9c894f7ef1d3102992c1df65360d
hamzajaradatlol <47142963+hamzajaradatlol@users.noreply.github.com>
2019-12-31 16:43:03 +0200

    Update gmailbomba.py

--- gmailbomba.py
+++ gmailbomba.py
@@ -1,8 +1,9 @@
 import  smtplib
 from colorama import Fore
+print(Fore.GREEN + "Default email: emailbombs0@gmail.com. Password: emailbombs@123
 ymail = input(Fore.RED + "Please write down your email: ")
 rmail = input("Please write down the victim's email: ")
-passwd = input(str("Please write down your password: "))
+passwd = input(str("Please write down your password: ")
 message = input("Message: ")
 server = smtplib.SMTP("smtp.gmail.com", 587)
 server.starttls()
